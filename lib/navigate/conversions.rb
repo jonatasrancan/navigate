ca8088cfe883abc129fb50f1282a61b0ccbe7f1f
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2015-05-27 20:13:44 -0300

    Refactor the gem struct

--- lib/navigate/conversions.rb
+++ lib/navigate/conversions.rb
@@ -1,18 +1,16 @@
-module Navigate
-  module Conversions
-    # degrees to radians
-    def to_radians(degrees)
-      degrees * Math::PI / 180.0
-    end
+class Float
+  # degrees to radians
+  def to_radians
+    self * Math::PI / 180.0
+  end
 
-    # radians to bearing
-    def to_bearing(radians)
-      (to_degrees(radians) + 360.0) % 360.0
-    end
+  # radians to bearing
+  def to_bearing
+    (self.to_degrees + 360.0) % 360.0
+  end
 
-    # radians to degree
-    def to_degrees(radians)
-      radians * 180 / Math::PI
-    end
+  # radians to degree
+  def to_degrees
+    self * 180 / Math::PI
   end
 end
